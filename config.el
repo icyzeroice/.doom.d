88f2ab2c05f7856f2933db6b2c64afbda222cddc
icyzeroice <ziv3@outlook.com>
2020-12-21 20:03:09 +0800

    update

--- config.el
+++ config.el
@@ -54,6 +54,8 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
+(setq ispell-dictionary "american")
+
 (load! "features/emacs")
 (load! "features/cnfonts")
 (load! "features/org-roam")
