072164528b7a530f1ec54e942fc10cdf072a6f62
didi <ziv3@outlook.com>
2021-12-08 20:00:03 +0800

    sync

--- features/cnfonts.el
+++ features/cnfonts.el
@@ -14,40 +14,43 @@
 
   (set-language-environment "UTF-8")
 
-  (pcase system-type
+  (if (display-graphic-p)
+	(pcase system-type
 
-    ('windows-nt (progn (set-face-attribute
-                         'default nil
-                         :font (font-spec :name "-outline-Consolas-bold-italic-normal-mono-*-*-*-*-c-*-iso10646-1"
-                                          :weight 'normal
-                                          :slant 'normal
-                                          :size 10.0))
-                        (dolist (charset '(kana han symbol cjk-misc bopomofo))
-                          (set-fontset-font
-                           (frame-parameter nil 'font)
-                           charset
-                           (font-spec :name "-outline-微软雅黑-normal-normal-normal-sans-*-*-*-*-p-*-iso10646-1"
-                                      :weight 'normal
-                                      :slant 'normal
-                                      :size 12.0)))))
+	    ('windows-nt (progn (set-face-attribute
+				 'default nil
+				 :font (font-spec :name "-outline-Consolas-bold-italic-normal-mono-*-*-*-*-c-*-iso10646-1"
+						  :weight 'normal
+						  :slant 'normal
+						  :size 10.0))
+				(dolist (charset '(kana han symbol cjk-misc bopomofo))
+				  (set-fontset-font
+				   (frame-parameter nil 'font)
+				   charset
+				   (font-spec :name "-outline-微软雅黑-normal-normal-normal-sans-*-*-*-*-p-*-iso10646-1"
+					      :weight 'normal
+					      :slant 'normal
+					      :size 12.0)))))
 
-    ('darwin (progn (set-face-attribute
-                     'default nil
-                     :font (font-spec :name "-*-Monaco-normal-normal-normal-*-*-*-*-*-m-0-iso10646-1"
-                                      :weight 'normal
-                                      :slant 'normal
-                                      :size 12.5))
-                    (dolist (charset '(kana han symbol cjk-misc bopomofo))
-                      (set-fontset-font
-                       (frame-parameter nil 'font)
-                       charset
-                       (font-spec :name "-*-Hiragino Sans GB-normal-normal-normal-*-*-*-*-*-p-0-iso10646-1"
-                                  :weight 'normal
-                                  :slant 'normal
-                                  :size 15.0)))))
+	    ('darwin (progn (set-face-attribute
+			     'default nil
+			     :font (font-spec :name "-*-Monaco-normal-normal-normal-*-*-*-*-*-m-0-iso10646-1"
+					      :weight 'normal
+					      :slant 'normal
+					      :size 12.5))
+			    (dolist (charset '(kana han symbol cjk-misc bopomofo))
+			      (set-fontset-font
+			       (frame-parameter nil 'font)
+			       charset
+			       (font-spec :name "-*-Hiragino Sans GB-normal-normal-normal-*-*-*-*-*-p-0-iso10646-1"
+					  :weight 'normal
+					  :slant 'normal
+					  :size 15.0)))))
 
-    (code (progn (cnfonts-mode)
-                 (message "Unhandled system-type %S" code)))))
+	    (code (progn (cnfonts-mode)
+			 (message "Unhandled system-type %S" code)))))
+
+  )
 
 
 (hyouka/cnfonts)
